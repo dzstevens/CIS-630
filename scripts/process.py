8773ff8c2fb699300cdb8a899f87cbd68978baf1
David Stevens <dstevens18@gmail.com>
2012-11-01 02:49:14 -0700

    refactored file name in process.py that was missed in last commit. Got rid of tabs in my spaces...

--- scripts/process.py
+++ scripts/process.py
@@ -1,26 +1,27 @@
-import pickle	
-	
+import pickle   
+
+
 def main():
-	keys = []
-	
-	with open('lookup.pkl', 'rb') as f:
-		keys = pickle.load(f)
-	
-	post = {}
-	with open('posterior.tsv') as f:
-		for l in f:
-			line = l.strip().split('\t')
-			node = keys[int(line[0])]
-			val = round(float(line[1]), 3)
-			if val == 0.001:
-				val = 0.0
-			elif val == 0.999:
-				val = 1.0
-			post[node] = str(val)
-				
-	with open('posterior_processed.tsv', 'w') as f:
-		for k in sorted(post):
-			f.write(k + '\t' + post[k] + '\n')	
+    keys = []
+    with open('lookup.pkl', 'rb') as f:
+        keys = pickle.load(f)
+
+    post = {}
+    with open('posterior_unprocessed.tsv') as f:
+        for l in f:
+            line = l.strip().split('\t')
+            node = keys[int(line[0])]
+            val = round(float(line[1]), 3)
+            if val == 0.001:
+                val = 0.0
+            elif val == 0.999:
+                val = 1.0
+            post[node] = str(val)
+
+    with open('posterior_processed.tsv', 'w') as f:
+        for k in sorted(post):
+            f.write(k + '\t' + post[k] + '\n')  
+
 
 if __name__ == '__main__':
-	main()
+    main()

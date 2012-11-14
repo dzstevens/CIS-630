93e821270ec1f1c2816437033626850881c305e3
Paul Elliott <dberquesitapprojects@gmail.com>
2012-11-13 16:18:55 -0800

    updated client

--- code/client.py
+++ code/client.py
@@ -2,28 +2,46 @@
 #needs to have an init => promptly pull and push
 
 import os
+from WatchDirectory import WatchDirectoryThread
+import socket
+from threading import Thread, Lock, Event
+
+class WriteThread(Thread):
+  def __init(self,broker,sock=None):
+    if sock is None:
+      self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    else:
+      self.sock = sock
+    self.broker = broker
+
+  def run(self):
+    print("Write thread is running")
+#send request to broker
+#block until response=ok
+#send thing
+#block until good
+  
+  def connect(self, host, port):
+    self.sock.connect((broker.host,broker.port))
+
+  def write_modification(self,modified_file):
+    print(modified_file," was modified")
+  def write_new_file(self,new_file):
+    print("New file: ", new_file)
+  def write_deleted_file(self,deleted_file):
+    print("Deleted file: ", deleted_file)
 
 class Client:
 
-  def __init__(self,broker,local_dir):
-    #self.write_socket
+  def __init__(self,local_dir,broker = None):
+
     #initiate connection to broker
     self.local_directory = local_dir
+    self.write_thread = WriteThread()
+    self.write_thread.run()
+    #self.read_thread
 
-  def watch_directory(self):
-    current_file_dict = dict([(file,os.stat(self.local_directory + file)) for file in os.listdir(self.local_directory)])
-    print "current", current_file_dict
-    while True:
-      try:
-        new_file_dict = dict([(file,os.stat(self.local_directory + file)) for file in os.listdir(self.local_directory)])
-      except os.error:
-        continue
-      if new_file_dict.keys() != current_file_dict.keys():
-        print "addition or deletion"
-      for file,last_modified in new_file_dict.items():
-        if file in current_file_dict.keys() and last_modified > current_file_dict[file]:
-          print file, " was modified"
-      current_file_dict = new_file_dict
+    self.watch_directory_thread = WatchDirectoryThread(self.local_directory,self.write_thread)
+    self.watch_directory_thread.run()
 
-x= Client('broker','/Users/krazkorean/Desktop/cis630/test/')
-x.watch_directory()
+x= Client('/Users/krazkorean/Desktop/cis630/test/')

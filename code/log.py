b9c34aed156f69f88aa5971ad1a72acd205f84dd
David Stevens <david.stevens@webfilings.com>
2012-11-18 02:46:28 -0800

    Changed debugs some more

--- code/log.py
+++ code/log.py
@@ -2,9 +2,10 @@ import time
 
 def log(what, *data):
     if len(data):
-        print time.localtime(), what,  "{"
+        t = time.localtime()
+        print('{}:{}:{}: \'{}\':').format(t.tm_hour, t.tm_min, t.tm_sec, what)
         for datum in data:
-            print repr(datum)
-        print "}"
+            print '\t'+repr(datum)
+        print
     else:
         print what

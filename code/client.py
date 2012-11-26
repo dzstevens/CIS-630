bc7e822527035a3123d13708327d2ce829312011
Paul Elliott <dberquesitapprojects@gmail.com>
2012-11-26 06:05:12 -0500

    init check and push for client, bugfixes for record keeping

--- code/client.py
+++ code/client.py
@@ -2,36 +2,107 @@
 Created on Nov 17, 2012
 
 @author: David Stevens
+
+Modified by: Paul Elliott (PE) Nov 26, 2012
+    Added get_datetime helper, LocalFilesEventHandler.initial_update_and_push
+    Modified to use record keeping (see db_utils.py)
+    Modified to run initial check for offline changes and push those changes
 '''
 
 import asynchat, asyncore, errno, logging, os, re, shutil, socket
+from datetime import datetime
 
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
 import constants
+from db_utils import ClientRecord
 
+def get_datetime(timestamp):
+    '''Converts a UTC timestamp to a datetime object and returns'''
+    (year,month,day),(hour,minute,second) = timestamp.split(' ')[0].split('-'),timestamp.split(' ')[1].split(':') #PE string splits FTW
+    if second.find('.') > -1:
+        second,microsecond = second.split('.')
+    else:
+        microsecond = 0
+    return datetime(int(year),int(month),int(day),int(hour),int(minute),int(second),int(microsecond))
 
 class LocalFilesEventHandler(FileSystemEventHandler):
     '''Handles events in the file system and passes it to the channel'''
 
-    def __init__(self, dirname):
+    def __init__(self, dirname, record_source, loglevel):
         FileSystemEventHandler.__init__(self)
         self.dirname = dirname
         self.changes = {}
         self.just_changed = {}
         self.valid = re.compile(r'^(.+/)*[^\./][^/~]*$')
+        self.record_source = record_source
+        self.loglevel = loglevel
+
+    def initial_update_and_push(self):
+        '''Checks for any updates since last snapshot of records, pushes all updates'''
+        ADD=0
+        DELETE=1 #PE local flags
+
+        record = ClientRecord(record_source,loglevel)
+        current_records = dict([ (filename,(sequencenum,get_datetime(timestamp))) for filename,sequencenum,timestamp in record.fetch_current_records() ])
+        logging.info('Checking initial records')
+        logging.debug('Current records: {}'.format(current_records))
+        
+        initial_changes = []
+        directory_walker = os.walk(self.dirname)
+        cur_directory,cur_subdirectories,cur_files = directory_walker.next()
+        while(True): #PE walk entire directory, add necessary changes to initial_changes
+            cur_directory = cur_directory if cur_directory.endswith('/') else cur_directory + '/'
+            logging.debug('Checking {}'.format(cur_directory))
+            for filename in cur_subdirectories + cur_files:
+                filename = os.path.relpath(cur_directory + filename, self.dirname)
+                modified_time = datetime.utcfromtimestamp(os.path.getmtime(self.dirname+filename))
+                logging.debug('  Evaluating {} modified on {}'.format(filename, modified_time.isoformat(' ')))
+                if filename in current_records:
+                    discard,record_timestamp = current_records.pop(filename)
+                    if modified_time > record_timestamp: #PE file/folder updated
+                        initial_changes.append((filename, ADD))
+                else: #PE file/folder added
+                    initial_changes.append((filename, ADD))
+            try:
+                cur_directory,cur_subdirectories,cur_files = directory_walker.next()
+            except StopIteration:
+                break
+        for filename in current_records: #PE file/folder deleted
+            initial_changes.append((filename, DELETE))
+
+        logging.info('Updating records and pushing initial changes')
+        logging.debug('Changes to push: {}'.format(initial_changes))
+        for filename,flag in initial_changes:
+            #print self.dirname+filename," is file? ",os.path.isfile(self.dirname+filename)
+            if record.update_sequencenum_or_create(filename) == -1:
+                logging.warning('Something went wrong.')
+            if flag == ADD:
+                change = constants.ADD_FILE if os.path.isfile(self.dirname+filename) else constants.ADD_FOLDER 
+            else:
+                change = constants.DELETE_FILE if os.path.isfile(self.dirname+filename) else constants.DELETE_FOLDER 
+            self.handle_change(self.dirname+filename, change)
+
     def handle_change(self, filename, change):
+        record = ClientRecord(record_source,loglevel)
         filename = os.path.relpath(filename, self.dirname)
         if self.valid.match(filename):
             logging.info('Change happened to {}'.format(filename))
             logging.debug('Data : {}'
                           ''.format(constants.DIRECTORY_FLAG_TO_NAME[change]))
+            #PE Q could we leverage local records here to prevent duplicate updates?
             if not self.just_changed.get(filename):
                 if filename not in self.changes:
+                    sequencenum = record.get_sequencenum(filename)
+                    logging.debug('{} sequencenum: {}'.format(filename,sequencenum))
+                    if sequencenum == -1:
+                      logging.warning('Aborting change')
+                      return
                     self.channel.push(filename + constants.DELIMITER +
                                       str(constants.REQUEST) +
                                       constants.TERMINATOR)
+                    #PE TODO must push sequencenum to broker (modify this and push)
                     logging.debug('Pushing: {} {}'.format(filename,
                                                           constants.REQUEST))
                 self.changes[filename] = change
@@ -60,12 +131,14 @@ class LocalFilesEventHandler(FileSystemEventHandler):
             self.handle_change(event.src_path, (constants.ADD_FILE))
 
     def on_moved(self, event):
+        #Or (|) logic handles whether file or folder
         self.handle_change(event.src_path, (constants.DELETE_FILE |
                                             event.is_directory))
         self.handle_change(event.dest_path, (constants.ADD_FILE |
                                              event.is_directory))
 
     def take_change(self, filename):
+        '''Dequeues a change from the change queue'''
         if filename in self.changes:
             change = self.changes[filename]
             del self.changes[filename]
@@ -90,14 +163,15 @@ class LocalFilesEventHandler(FileSystemEventHandler):
 class BrokerChannel(asynchat.async_chat):
     '''The channel that deals with sending and receiving to the broker.'''
 
-    def __init__(self, dirname, host, port):
+    def __init__(self, dirname, record_source, host, port, loglevel):
         asynchat.async_chat.__init__(self)
         self.dirname = dirname
-        self.process_data = self.process_message
-        self.received_data = []
+        self.process_data = self.process_message #input stream handler
+        self.received_data = [] #buffer
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((host, port))
         self.set_terminator(constants.TERMINATOR)
+        self.record = ClientRecord(record_source,loglevel) #PE connect to record and pass to event_handler and channel
 
     def push(self, data):
         logging.info('Pushed Data')
@@ -122,13 +196,13 @@ class BrokerChannel(asynchat.async_chat):
         logging.debug('Data : {}'.format(repr(token)))
         msg[1] = int(msg[1])
         filename, flag = msg[:2]
-        logging.info('Recieved Message with '
+        logging.info('Received Message with '
                      'flag {}'.format(constants.DIRECTORY_FLAG_TO_NAME[flag]))
         logging.debug('Data : {}'.format(repr(msg)))
-        if flag == constants.REQUEST:
+        if flag == constants.REQUEST: #broker responded with an ACK to push request
             self.handle_push_change(filename)
         else:
-            self.handle_receive_change(msg)
+            self.handle_receive_change(msg) #broker sending change
 
     def process_file(self):
         token = self.get_token()
@@ -162,10 +236,15 @@ class BrokerChannel(asynchat.async_chat):
         logging.info('Connected')
 
     def handle_push_change(self, filename):
-        flags = self.event_handler.take_change(filename)
+        '''dequeues a change and pushes it to the broker'''
+        #flags = self.event_handler.take_change(filename)
+        flags = self.event_handler.take_change(filename) #pull next change from file handler
         for flag in flags:
+            retval = self.record.update_record(filename,flag) #PE increment the sequence number for this file
+            if retval == -1:
+              logging.warning('Something went wrong, could not update record: {}'.format(filename))
             self.push(filename + constants.DELIMITER + str(flag))
-            if flag == constants.ADD_FILE:
+            if flag == constants.ADD_FILE: #push size as well for files
                 self.push(constants.DELIMITER +
                           str(os.stat(self.dirname + filename).st_size) +
                           constants.TERMINATOR)
@@ -197,9 +276,19 @@ class BrokerChannel(asynchat.async_chat):
                              'changed'.format(filename))
                 self.event_handler.just_changed[filename] = 'Adding'
                 self.remaining_size = int(msg[2])
-                self.process_data = self.process_file
-                self.set_terminator(min(self.remaining_size,
-                                        constants.CHUNK_SIZE))
+                if self.remaining_size == 0: #PE handle empty files, which will otherwise hang badly
+                  logging.debug('Empty file, closing {} and marking done being changed'.format(filename))
+                  self.event_handler.just_changed[filename] = 'Done'
+                  self.file.close()
+                else:
+                  self.process_data = self.process_file
+                  self.set_terminator(min(self.remaining_size,
+                                          constants.CHUNK_SIZE))
+            retval = self.record.update_sequencenum_or_create(filename) #PE modify: updates seqnum for record, add: creates new record for the added file/folder
+            logging.debug('updated record, returned {}'.format(retval))
+            if retval == -1:
+              logging.warning('Error updating records')
+            #PE TODO retrieve sequencenum and use to update
         except OSError as e:
             if e.errno == errno.EEXIST:
                 logging.warning(str(e))
@@ -222,11 +311,17 @@ class BrokerChannel(asynchat.async_chat):
                              'changed'.format(filename))
                 self.event_handler.just_changed[filename] = 'Done'
                 shutil.rmtree(self.dirname + filename)
+                retval = self.record.delete_directory_records(filename) #PE delete directory from records
+                if retval == -1:
+                  logging.warning('Error updating records')
             else:
                 logging.info('Mark {} as being just '
                              'changed'.format(filename))
                 self.event_handler.just_changed[filename] = 'Done'
                 os.remove(self.dirname + filename)
+                retval = self.record.delete_record(filename) #PE delete file from records
+                if retval == -1:
+                  logging.warning('Error updating records')
         except OSError as e:
             if e.errno == errno.ENOENT:
                 logging.warning(str(e))
@@ -282,10 +377,11 @@ if __name__ == '__main__':
     host = constants.HOST
     port = constants.PORT
     loglevel = logging.INFO
+    record_source = None
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'd:h:p:l:v:',
+        opts, args = getopt.getopt(sys.argv[1:], 'd:h:p:l:v:r:',
                                    ['dir=', 'host=', 'port=', 'logging=',
-                                    'verbose='])
+                                    'verbose=','record=']) #PE added a -r/--record arg
     except getopt.GetoptError:
         logging.warning('The system arguments are incorrect')
         sys.exit(2)
@@ -301,19 +397,21 @@ if __name__ == '__main__':
             port = int(arg)
         elif opt in ('-v', '-l', '--logging'):
             loglevel = 10 * (6 - int(arg))
+        elif opt in ('-r', '--record'): #PE handle record arg
+            record_source = arg
 
     if not dirname.endswith('/'):
         dirname += '/'
-
+    if not record_source: #PE create random record source if none provided
+      import random
+      record_source='defaultclient{}'.format(random.randint(1,1000))
     logging.basicConfig(format=constants.LOG_FORMAT, level=loglevel)
-
     observer = Observer()
-    event_handler = LocalFilesEventHandler(dirname)
-    channel = BrokerChannel(dirname, host, port)
-
+    event_handler = LocalFilesEventHandler(dirname, record_source, loglevel)
+    channel = BrokerChannel(dirname, record_source, host, port, loglevel) #PE pass record_source to both..can't pass sqlite conn between threads
     event_handler.channel = channel
     channel.event_handler = event_handler
+    event_handler.initial_update_and_push() #PE check for offline changes and push all
     observer.schedule(event_handler, dirname, True)
-
     observer.start()
-    asyncore.loop()
+    asyncore.loop() 
--- code/db_utils.py
+++ code/db_utils.py
@@ -1,133 +1,170 @@
+'''
+Created on Nov 26, 2012
+
+@author: Paul Elliott
+'''
+
 import sqlite3
-import constants
 import logging
 import sys
 import os
+from datetime import datetime
+
+import constants
 
 class ClientRecord:
-  '''Handles all transactions between client and local record'''
+    '''Handles all transactions between client and local record'''
 
-  def __init__(self,record_source,loglevel = 1):
-    '''Create DB, connection, table'''
-    logging.basicConfig(format=constants.LOG_FORMAT, level=loglevel)
-    try:
-      self.conn = sqlite3.connect(constants.CLIENT_RECORD_DIR + record_source)
-      self.cursor = self.conn.cursor()
-      self.cursor.execute("CREATE TABLE IF NOT EXISTS records(id INTEGER PRIMARY KEY AUTOINCREMENT, filename TEXT NOT NULL UNIQUE, sequencenum INTEGER NOT NULL, timestamp DEFAULT CURRENT_TIMESTAMP);")
-      #PE filename: on conflict replace? or handle in code?
-    except sqlite3.Error, e:
-      logging.warning('Unable to connect to local record: {}'.format(constants.CLIENT_RECORD_DIR + record_source))
-      logging.debug(e.message)
-      sys.exit(2)
-    logging.info('Client connected to local record: {}'.format(constants.CLIENT_RECORD_DIR + record_source))
+    def __init__(self,record_source,loglevel = 1):
+        '''Create DB, connection, table'''
+        logging.basicConfig(format=constants.LOG_FORMAT, level=loglevel)
+        try:
+            self.conn = sqlite3.connect(constants.CLIENT_RECORD_DIR + record_source)
+            self.cursor = self.conn.cursor()
+            self.cursor.execute("CREATE TABLE IF NOT EXISTS records(id INTEGER PRIMARY KEY AUTOINCREMENT, filename TEXT NOT NULL UNIQUE, sequencenum INTEGER NOT NULL, timestamp DEFAULT CURRENT_TIMESTAMP);")
+            #PE filename: on conflict replace? or handle in code?
+        except sqlite3.Error, e:
+            logging.warning('Record : Unable to connect to local record: {}'.format(constants.CLIENT_RECORD_DIR + record_source))
+            logging.debug(e.message)
+            sys.exit(2)
+        logging.debug('Record : Client connected to local record: {}'.format(constants.CLIENT_RECORD_DIR + record_source))
 
-  def get_sequencenum(self,filename):
-    '''Retrieve the sequence number of the given file, return -1 on failure'''
-    logging.debug("getting seqnum for: {}".format(filename))
-    try:
-      sequence_num = self.cursor.execute("SELECT sequencenum FROM records WHERE filename='{}';".format(filename)).fetchone()
-    except sqlite3.Error, e:
-      logging.warning('Failed to retrieve sequencenum for: {}'.format(filename))
-      logging.debug(e.message)
-      return -1
+    def get_sequencenum(self,filename):
+        '''Retrieve the sequence number of the given file, return -1 on failure'''
+        logging.debug('Record : Getting seqnum for: {}'.format(filename))
+        try:
+            sequence_num = self.cursor.execute("SELECT sequencenum FROM records WHERE filename='{}';".format(filename)).fetchone()
+        except sqlite3.Error, e:
+            logging.warning('Record : Failed to retrieve sequencenum for: {}'.format(filename))
+            logging.debug(e.message)
+            return -1
     
-    if sequence_num: 
-      return sequence_num[0]
-    else:
-      logging.debug("{} not in records, creating".format(filename))
-      return self.create_record(filename)
+        if sequence_num: 
+            return sequence_num[0]
+        else:
+            logging.debug('Record : {} not in records, creating'.format(filename))
+            return self.create_record(filename)
 
-  def update_sequencenum(self, filename, new_sequencenum=None):
-    '''Update the sequence number of the given record, return 0 on success, -1 on fail'''
-    logging.debug("updating sequencenum for {}, using {}".format(filename,new_sequencenum))
-    try:
-      sequencenum = self.cursor.execute("SELECT sequencenum FROM records WHERE filename='{}';".format(filename)).fetchone()
-    except sqlite3.Error, e:
-      logging.warning('Something went wrong')
-      logging.debug(e.message)
-      return -1
-    if sequencenum:
-      try:
-        logging.debug("record exists, updating seqnum")
-        sequencenum = new_sequencenum if new_sequencenum else sequencenum[0]+1 #either update to new sequence number or increment
-        self.cursor.execute("UPDATE records SET sequencenum={} WHERE filename='{}';".format(sequencenum,filename))
-        self.conn.commit()
-        return 0
-      except sqlite3.Error, e:
-        logging.warning('Failed to update sequencenum')
-        logging.debug(e.message)
-        return -1
-    else:
-      logging.debug("{} not in records, creating with seqnum: {}".format(filename,sequencenum))
-      return self.create_record(filename,new_sequencenum)
+    def update_sequencenum_or_create(self, filename, new_sequencenum=None):
+        '''Update the sequence number of the given record, return 0 on success, -1 on fail'''
+        logging.debug('Record : Updating sequencenum for {}, using seqnum {}'.format(filename,new_sequencenum))
+        try:
+            sequencenum = self.cursor.execute("SELECT sequencenum FROM records WHERE filename='{}';".format(filename)).fetchone()
+        except sqlite3.Error, e:
+            logging.warning('Record : Something went wrong')
+            logging.debug(e.message)
+            return -1
+        if sequencenum:
+            try:
+                logging.debug('Record : Record exists, updating seqnum')
+                sequencenum = new_sequencenum if new_sequencenum else sequencenum[0]+1 #either update to new sequence number or increment
+                self.cursor.execute("UPDATE records SET sequencenum={}, timestamp='{}' WHERE filename='{}';".format(sequencenum,datetime.utcnow().isoformat(' '),filename))
+                self.conn.commit()
+                return 0
+            except sqlite3.Error, e:
+                logging.warning('Record : Failed to update sequencenum')
+                logging.debug(e.message)
+                return -1
+        else:
+            logging.debug('Record : {} not in records, creating with seqnum: {}'.format(filename,sequencenum))
+            return self.create_record(filename,new_sequencenum)
 
-  def create_record(self, filename, sequencenum=0):
-    '''Create a new record, return 0 on success, -1 on failure'''
-    logging.debug("creating record for {}".format(filename))
-    try:
-      self.cursor.execute("INSERT INTO records(filename,sequencenum) VALUES('{}',{});".format(filename,sequencenum))
-      self.conn.commit()
-      return 0
-    except sqlite3.Error, e:
-      logging.warning('Failed to create record: {}'.format(filename))
-      logging.debug(e.message)
-      return -1
+    def create_record(self, filename, sequencenum=None):
+        '''Create a new record, return 0 on success, -1 on failure'''
+        logging.debug('Record : Creating record for {}'.format(filename))
+        if not sequencenum:
+            sequencenum = 0
+        try:
+            self.cursor.execute("INSERT INTO records(filename,sequencenum) VALUES('{}',{});".format(filename,sequencenum))
+            self.conn.commit()
+            return 0
+        except sqlite3.Error, e:
+            logging.warning('Record : Failed to create record: {}'.format(filename))
+            logging.debug(e.message)
+            return -1
 
-  def delete_directory_records(self,filename):
-    '''Deletes an entire directory's records, return 0 on success, -1 on failure'''
-    logging.debug("deleting directory: {}".format(filename))
-    directory_to_delete = filename.split('/')[0] + '/%'
-    try:
-      self.cursor.execute("DELETE FROM records WHERE filename='{}' or filename LIKE '{}';".format(filename,directory_to_delete))
-      self.conn.commit()
-      return 0
-    except sqlite3.Error, e:
-      logging.warning('Failed to delete directory record: {}'.format(filename))
-      logging.debug(e.message)
-      return -1
+    def delete_directory_records(self,filename):
+        '''Deletes an entire directory's records, return 0 on success, -1 on failure'''
+        logging.debug('Record : Deleting directory: {}'.format(filename))
+        directory_to_delete = filename.split('/')[0] + '/%'
+        try:
+            self.cursor.execute("DELETE FROM records WHERE filename='{}' or filename LIKE '{}';".format(filename,directory_to_delete))
+            self.conn.commit()
+            return 0
+        except sqlite3.Error, e:
+            logging.warning('Record : Failed to delete directory record: {}'.format(filename))
+            logging.debug(e.message)
+            return -1
 
-  def delete_record(self,filename):
-    '''Deletes a record, return 0 on success, -1 on failure'''
-    logging.debug("deleting record: {}".format(filename))
-    try:
-      self.cursor.execute("DELETE FROM records WHERE filename='{}';".format(filename))
-      self.conn.commit()
-      return 0
-    except sqlite3.Error, e:
-      logging.warning('Failed to delete record: {}'.format(filename))
-      logging.debug(e.message)
-      return -1
+    def delete_record(self,filename):
+        '''Deletes a record, return 0 on success, -1 on failure'''
+        logging.debug('Record : Deleting record: {}'.format(filename))
+        try:
+            self.cursor.execute("DELETE FROM records WHERE filename='{}';".format(filename))
+            self.conn.commit()
+            return 0
+        except sqlite3.Error, e:
+            logging.warning('Record : Failed to delete record: {}'.format(filename))
+            logging.debug(e.message)
+            return -1
+
+    def update_record(self, filename, flag):
+        '''Handles updating a record based on flag'''
+        if flag == constants.ADD_FILE or flag == constants.ADD_FOLDER:
+            retval = self.update_sequencenum_or_create(filename)
+        elif flag == constants.DELETE_FILE:
+            retval = self.delete_record(filename)
+        elif flag == constants.DELETE_FOLDER:
+            retval = self.delete_directory_records(filename)
+        return retval
+
+    def fetch_current_records(self):
+        '''Fetches all current records'''
+        try:
+            return self.cursor.execute("SELECT filename,sequencenum,timestamp FROM records").fetchall()
+        except sqlite3.Error, e:
+            logging.warning('Failed to fetch records')
+            logging.debug(e.message)
+            return []
 
 if __name__ == '__main__':
-  import sys
-  if len(sys.argv) < 2:
-    print "USAGE: python db_utils.py <record>"
-    sys.exit(1)
-  record_source = sys.argv[1]
-  test_client_record = ClientRecord(record_source,10)
-  input = ""
-  while input != "EXIT":
-    input = raw_input("Enter 'C' to create, 'DD' to delete dir, 'D' to delete, 'G' to get seqnum, 'U' to update seqnum, 'EXIT' to quit: ")
-    if input == 'C':
-      filename = raw_input("Enter filename to create: ")
-      test_client_record.create_record(filename)
-    if input == 'DD':
-      filename = raw_input("Enter directory to delete: ")
-      test_client_record.delete_directory_records(filename)
-    if input == 'D':
-      filename = raw_input("Enter filename to delete: ")
-      test_client_record.delete_record(filename)
-    if input == 'G':
-      filename = raw_input("Enter filename to retrieve: ")
-      print "Retrieved: ", test_client_record.get_sequencenum(filename)
-    if input == 'U':
-      filename = raw_input("Enter filename to update: ")
-      sequencenum = raw_input("Enter optional seqnum, or None: ")
-      if sequencenum == "None":
-        test_client_record.update_sequencenum(filename)
-      else:
-        test_client_record.update_sequencenum(filename,int(sequencenum))
-    if input == 'EXIT':
-      print "QUITTING"
-      sys.exit(0)
-    
+    import sys
+    if len(sys.argv) < 2:
+        print "USAGE: python db_utils.py <record>"
+        sys.exit(1)
+    record_source = sys.argv[1]
+    test_client_record = ClientRecord(record_source,10)
+    input = ""
+    while input != "EXIT":
+        input = raw_input("Enter 'C/DD/D/G/U/UR/EXIT' to create/delete dir/delete/get seqnum/update seqnum/update record/quit: ")
+        if input == 'C':
+            filename = raw_input("Enter filename to create: ")
+            test_client_record.create_record(filename)
+        if input == 'DD':
+            filename = raw_input("Enter directory to delete: ")
+            test_client_record.delete_directory_records(filename)
+        if input == 'D':
+            filename = raw_input("Enter filename to delete: ")
+            test_client_record.delete_record(filename)
+        if input == 'G':
+            filename = raw_input("Enter filename to retrieve: ")
+            print "Retrieved: ", test_client_record.get_sequencenum(filename)
+        if input == 'U':
+            filename = raw_input("Enter filename to update: ")
+            sequencenum = raw_input("Enter optional seqnum, or None: ")
+            if sequencenum == "None":
+                test_client_record.update_sequencenum_or_create(filename)
+            else:
+                test_client_record.update_sequencenum_or_create(filename,int(sequencenum))
+        if input == 'UR':
+            filename = raw_input("Enter filename to update: ")
+            flags = {'REQUEST':constants.REQUEST,'ADD_FILE':constants.ADD_FILE,'DELETE_FILE':constants.DELETE_FILE,'ADD_FOLDER':constants.ADD_FOLDER,'DELETE_FOLDER':constants.DELETE_FOLDER}
+            flag = raw_input("Enter flag: ")
+            try:
+                test_client_record.update_record(filename,flags[flag])
+            except:
+                print "invalid flag"
+        if input == 'EXIT':
+            print "QUITTING"
+            sys.exit(0)
+

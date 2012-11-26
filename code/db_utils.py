46d28d0f98ab3a1fb9ea31e7924210ed64e2223c
Paul Elliott <dberquesitapprojects@gmail.com>
2012-11-25 21:42:44 -0500

    bugfixes for db_utils

--- code/db_utils.py
+++ code/db_utils.py
@@ -25,14 +25,14 @@ class ClientRecord:
     '''Retrieve the sequence number of the given file, return -1 on failure'''
     logging.debug("getting seqnum for: {}".format(filename))
     try:
-      sequence_num = self.cursor.execute("SELECT sequencenum FROM records WHERE filename='{}';".format(filename)).fetchone()[0]
+      sequence_num = self.cursor.execute("SELECT sequencenum FROM records WHERE filename='{}';".format(filename)).fetchone()
     except sqlite3.Error, e:
       logging.warning('Failed to retrieve sequencenum for: {}'.format(filename))
       logging.debug(e.message)
       return -1
     
     if sequence_num: 
-      return sequence_num
+      return sequence_num[0]
     else:
       logging.debug("{} not in records, creating".format(filename))
       return self.create_record(filename)
@@ -41,7 +41,7 @@ class ClientRecord:
     '''Update the sequence number of the given record, return 0 on success, -1 on fail'''
     logging.debug("updating sequencenum for {}, using {}".format(filename,new_sequencenum))
     try:
-      sequencenum = self.cursor.execute("SELECT sequencenum FROM records WHERE filename='{}';".format(filename)).fetchone()[0]
+      sequencenum = self.cursor.execute("SELECT sequencenum FROM records WHERE filename='{}';".format(filename)).fetchone()
     except sqlite3.Error, e:
       logging.warning('Something went wrong')
       logging.debug(e.message)
@@ -49,7 +49,7 @@ class ClientRecord:
     if sequencenum:
       try:
         logging.debug("record exists, updating seqnum")
-        sequencenum = new_sequencenum if new_sequencenum else sequencenum+1
+        sequencenum = new_sequencenum if new_sequencenum else sequencenum[0]+1 #either update to new sequence number or increment
         self.cursor.execute("UPDATE records SET sequencenum={} WHERE filename='{}';".format(sequencenum,filename))
         self.conn.commit()
         return 0
@@ -75,8 +75,8 @@ class ClientRecord:
 
   def delete_directory_records(self,filename):
     '''Deletes an entire directory's records, return 0 on success, -1 on failure'''
+    logging.debug("deleting directory: {}".format(filename))
     directory_to_delete = filename.split('/')[0] + '/%'
-    logging.debug("deleting directory: {}".format(directory_to_delete))
     try:
       self.cursor.execute("DELETE FROM records WHERE filename='{}' or filename LIKE '{}';".format(filename,directory_to_delete))
       self.conn.commit()
